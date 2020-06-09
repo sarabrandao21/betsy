7929b059ec0ec971ecf5033b7f0d41d7209086b2
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-09 16:53:18 -0700

    dry up some controller code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
+
+  before_action :find_product, only: [:show, :edit, :update]
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find_by(id: params[:id])
+    # @product = Product.find_by(id: params[:id])
   end
 
 
@@ -16,7 +18,7 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     if @product.save
       flash[:success] = "Successfully created #{@product.name}"
-      redirect_to product_path(@product)
+      redirect_to products_path
     else
       flash[:error] = "Couldn't create product!"
       render :new, status: :bad_request
@@ -24,10 +26,11 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find_by(id: params[:id])
+    # @product = Product.find_by(id: params[:id])
   end
 
   def update
+    # @product = Product.find_by(id: params[:id])
     if @product.update(product_params)
       flash[:success] = "Successfully edited #{@product.name}"
       redirect_to product_path(@product)
@@ -41,5 +44,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:name, :description, :price, :image, :stock, :rating)
+  end
+
+  def find_product
+    @product = Product.find_by(id: params[:id])
   end
 end
