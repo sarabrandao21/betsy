e7342b0510699456151c85acd74cc64233ac3077
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-10 10:21:49 -0700

    Added Merchant.build_from_github

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,17 @@
 class Merchant < ApplicationRecord
-    has_many :products
+  has_many :products
 
-    validates :username, uniqueness: true, presence: true
-    validates :email, uniqueness: true, presence: true
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+
+  def self.build_from_github(auth_hash)
+    merchant = Merchant.new
+    merchant.uid = auth_hash[:uid]
+    merchant.provider = "github"
+    merchant.username = auth_hash["info"]["nickname"]
+    merchant.email = auth_hash["info"]["email"]
+    merchant.avatar = auth_hash["info"]["image"]
+    
+    return user
+  end
 end
