3c344307916a03c3564ea547e5040d23aac2c99f
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-10 06:56:28 -0700

    Added basic index and show routes for merchants

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,11 @@
 class MerchantsController < ApplicationController
+  
+  def index
+    @merchants = Merchant.all
+  end
+
+  def show
+    @merchant = Merchant.find_by(id: params[:id])
+    render_404 unless @user
+  end
 end
