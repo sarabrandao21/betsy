6127a41a3b82441c63b4b1d0b777d217bb45f7ee
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-06-10 15:24:33 -0700

    new category can be created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,31 @@
 class CategoriesController < ApplicationController
 
+    before_action :find_category, only: [:show, :edit, :update, :destroy]
 
     def index 
         @categories = Category.all
     end 
 
     def show
+        render_404 unless @category
     end 
 
     def new 
+        @category = Category.new
     end 
 
     def create 
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:status] = :success
+            flash[:result_text] = "Successfully created #{@category.name} #{@category.id}"
+            redirect_to categories_path
+        else
+            flash[:status] = :failure
+            flash[:result_text] = "Could not create #{@category.name}"
+            flash[:messages] = @category.errors.messages
+            render :new, status: :bad_request
+        end
     end 
 
 
@@ -22,7 +36,7 @@ class CategoriesController < ApplicationController
         params.require(:category).permit(:name)
     end
 
-    def find_product
+    def find_category
         @category = Category.find_by(id: params[:id])
     end
 end
