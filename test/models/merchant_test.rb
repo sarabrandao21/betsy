787753c58ae2a2641818b37fba87b70eec26ba01
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-10 06:23:46 -0700

    Added validation tests for Merchant#username and email

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 describe Merchant do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe "validations" do
+    it "requires a username" do
+      new_merchant = Merchant.new(email: "gmail@gmail.com")
+      expect(new_merchant.valid?).must_equal false
+      expect(new_merchant.errors.messages).must_include :username
+    end
+
+    it "requires a unique username" do
+      username = Merchant.first.username
+      new_merchant = Merchant.new(username: username, email: "gmail@gmail.com")
+
+      result = new_merchant.save
+      expect(result).must_equal false
+      expect(new_merchant.errors.messages).must_include :username
+    end
+
+    it "requires an email" do
+      new_merchant = Merchant.new(username: "Chris")
+      expect(new_merchant.valid?).must_equal false
+      expect(new_merchant.errors.messages).must_include :email
+    end
+
+    it "requires a unique email" do
+      email = Merchant.first.email
+      new_merchant = Merchant.new(username: "Chris", email: email)
+
+      result = new_merchant.save
+      expect(result).must_equal false
+      expect(new_merchant.errors.messages).must_include :email
+    end
+  end
+
 end
