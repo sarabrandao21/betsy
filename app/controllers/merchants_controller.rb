564fa326afb2c5cac66fedca8ec2e7ade0b73b58
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-10 09:54:07 -0700

    Added show view for merchants

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,6 +6,6 @@ class MerchantsController < ApplicationController
 
   def show
     @merchant = Merchant.find_by(id: params[:id])
-    render_404 unless @user
+    render_404 unless @merchant
   end
 end
