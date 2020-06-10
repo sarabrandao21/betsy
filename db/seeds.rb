6f2277f8bce10c6d934f64fa24289d98bf5db036
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-06-09 22:15:22 -0700

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+merchant_file = Rails.root.join("db", "merchant_seeds.csv")
+
+CSV.foreach(merchant_file, headers: true, header_converters: :symbol, converters: :all) do |row|
+  data = Hash[row.headers.zip(row.fields)]
+  puts data
+  Merchant.create!(data)
+end
+
+product_file = Rails.root.join("db", "product_seeds.csv")
+
+CSV.foreach(product_file, headers: true, header_converters: :symbol, converters: :all) do |row|
+    data = Hash[row.headers.zip(row.fields)]
+    puts data
+    Product.create!(data)
+end
