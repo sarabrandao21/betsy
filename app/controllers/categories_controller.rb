312b2f685eec3ed2a4a327acd2beef151b16b6b0
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-06-10 15:01:25 -0700

    validation for category, name is unique and presente

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,27 @@
 class CategoriesController < ApplicationController
+
+
     def index 
         @categories = Category.all
     end 
 
     def show
-
     end 
 
+    def new 
+    end 
+
+    def create 
+    end 
+
+
+
     private 
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
     def find_product
         @category = Category.find_by(id: params[:id])
     end
