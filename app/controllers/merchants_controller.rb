6c7ca15dc4cc448a97221f39b74da216198333d9
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-10 14:10:20 -0700

    Added MerchantsController#create

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -8,4 +8,25 @@ class MerchantsController < ApplicationController
     @merchant = Merchant.find_by(id: params[:id])
     render_404 unless @merchant
   end
+
+  def create
+    auth_hash = request.env["omniauth.auth"]
+
+    merchant = Merchant.find_by(uid: auth_hash[:uid], provider: "github")
+    if merchant
+      flash[:success] = "Successfully created #{@merchant.username}"
+    else
+      merchant = Merchant.build_from_github(auth_hash)
+
+      if merchant.save
+        flash[:success] = "Logged in as new merchant #{@merchant.username}"
+      else
+        flash[:error] = "Could not create new merchant account: #{merchant.errors.messages}"
+        return redirect_to root_path
+      end
+    end
+
+    session[:merchant_id] = merchant.id
+    return redirect_to root_path
+  end
 end
