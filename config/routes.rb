ed320876dc4846636c03cd4bc2e949d3739ea266
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-10 23:30:13 -0700

    added review on product show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products
+  resources :reviews, only: [:create]
+
   root 'homepage#index'
   
   resources :merchants, only: [:index, :show]
