db28ea7abbc7a49d85411ba50a56463f94862f84
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-12 09:43:03 -0700

    fix reviews controller create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,12 @@
 class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
-
-    respond_to do |format|
-      # rendering js partials
-      format.js {
-        if @review.save
-          @reviews = Review.where(product_id: @review.product_id)
-          render 'reviews/create'
-        else
-          # unable to save
-          flash[:error] = @review.error.messages
-          redirect_to product_path(@review.product_id)
-        end
-      }
+    if @review.save
+      flash[:success] = "Thank you for your review on the product!"
+      redirect_back fallback_location: root_path
+    else
+      flash[:error] = "Cannot create review...."
+      redirect_back fallback_location: root_path
     end
   end
 
