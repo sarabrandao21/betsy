6ce72e4c491a819c4b3aa3ae866b34aafd4ab708
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-11 20:02:59 -0700

    Added basic empty cart functionality

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find_by(id: session[:order_id])
-    
   end
 
   def create
@@ -27,5 +26,24 @@ class OrdersController < ApplicationController
       
       session[:order_id] = order.id
     end
+  end
+
+  def destroy
+    @order = Order.find_by(id: params[:id])
+
+    if @order.nil?
+      head :not_found
+      return
+    end
+
+    @order.order_items.each do |order_item|
+      order_item.destroy
+    end
+
+    @order.destroy
+    flash[:success] = "We removed all items from your shopping cart" 
+
+    session[:order_id] = nil
+    redirect_to root_path
   end
 end
