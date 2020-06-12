13f0ce68d13920bd7bc7a8c4b8cdc522847d109d
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-12 16:32:30 -0700

    fixed that merchant can't review own product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,19 @@
 class ReviewsController < ApplicationController
+  before_action :find_merchant
+
   def create
     @review = Review.new(review_params)
-    if @review.save
-      flash[:success] = "Thank you for your review on the product!"
+    if (@login_merchant == @review.product.merchant)
+      flash[:error] = "So sorry but you can't review your own product..."
       redirect_back fallback_location: root_path
     else
-      flash[:error] = "Cannot create review...."
-      redirect_back fallback_location: root_path
+      if @review.save
+        flash[:success] = "Thank you for your review on the product!"
+        redirect_back fallback_location: root_path
+      else
+        flash[:error] = "Cannot create review...."
+        redirect_back fallback_location: root_path
+      end
     end
   end
 
