18de241f86d03f5743448dfdc9214ad2602c1880
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-11 17:04:06 -0700

    Added basic logic for orders create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,25 @@
 class OrdersController < ApplicationController
+  def create
+    if session[:order_id]
+      product = Product.find_by(id: params[:id])
+
+      order = Order.find_by(id: session[:order_id])
+      
+      order_item = OrderItem.create(
+        order: order,
+        product: product)
+    else 
+      product = Product.find_by(id: params[:id])
+
+      order = Order.create
+      id = order.id
+      order = Order.find_by(id: id)
+
+      order_item = OrderItem.create(
+        order: order,
+        product: product)
+      
+      session[:order_id] = order.id
+    end
+  end
 end
