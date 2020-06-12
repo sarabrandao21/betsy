f7676f035ff15d0839e29e5a2e0f2b5d3690d81b
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-06-11 18:05:55 -0700

    deleted get all categories method

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,9 +2,4 @@ class Category < ApplicationRecord
     has_and_belongs_to_many :products, dependent: :destroy
     validates :name, uniqueness: true, presence: true
     
-    def self.get_categories 
-        list_categories = Category.all
-        names = list_categories.map { |category| category.name }
-        return names
-    end 
 end
