0d486ffe00935afd3555931e68df5e93989f6161
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-06-14 16:48:46 -0700

    taxes, subtotal and purchase_total methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,19 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
   def subtotal 
-    #
-    self.order_items.map 
+    total = 0 
+    self.order_items.map { |order_item| total += order_item.total_price_qty  }
+    return total.round(2)
   end 
 
   def taxes 
+    subtotal = self.subtotal()
+    taxes = subtotal * 0.10
+    return taxes.round(2) 
   end 
 
   def purchase_total 
+    return total = (self.subtotal() + self.taxes()).round(2)
   end 
 
 end
