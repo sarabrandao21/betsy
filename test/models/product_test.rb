dbbb724e1a6d924b8af6f2b8639e92590fbbd908
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-15 20:28:08 -0700

    fixed product model test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe Product do
   let(:product_with_category) { products(:yogamat) }
   before do 
-    @new_product = Product.new(name:"Air Force Ones", price:150, merchant_id: merchants(:sharon).id)
+    @new_product = Product.new(name:"Air Force Ones", price:150, merchant_id: merchants(:sharon).id, stock: 25)
     
   end
 
@@ -54,27 +54,28 @@ describe Product do
     expect(@new_product.errors.messages).must_include :price
     expect(@new_product.errors.messages[:price]).must_equal ["is not a number"]
   end
+
+  it "recieves error if Stock is missing" do
+    @new_product.stock = nil
+  
+    expect(@new_product.valid?).must_equal false
+    expect(@new_product.errors.messages).must_include :stock
+    expect(@new_product.errors.messages[:stock]).must_equal ["can't be blank", "is not a number"]
+  end
 end
   describe "custom methods" do
 
-    describe "top ten popular products" do
-      it "return the top ten works for each media" do
+    describe "self.popular_products " do
+      it  "return the top ten products when less than 12"do
         top_products = Product.popular_products
-        expect(top_products.length).must_equal 10
-      end
-
-      it "return the top ten products when less than ten" do
-        removed_item = products(:juice)
-        removed_item.destroy
-        top_products = Product.popular_products
-        expect(top_products.length).must_equal 9
+        expect(top_products.length).must_equal 11
         expect(top_products.length).wont_be_nil
       end
 
-      it "will return empty array if no Products" do
-        Product.destroy_all
+      it "return the top 12 popular products" do
+        Product.create!(name: "WaterSport Gear", price: 150, merchant: merchants(:sharon), stock: 20)
         top_products = Product.popular_products
-        expect(top_products).must_equal []
+        expect(top_products.length).must_equal 12
       end
     end
   end
