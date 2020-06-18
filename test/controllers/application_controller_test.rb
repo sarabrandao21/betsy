82c56366c4af5a67ab05ff98eac05580cac1db35
Sharon Cheung <keikei1128@g.ucla.edu>
2020-06-18 00:03:56 -0700

    fixed order test

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,12 +1,14 @@
 require "test_helper"
 
 describe ApplicationController do
+    # Setup Anonymous Controller for testing
   describe 'find_merchant' do
     it 'will save a login merchant id to session so it will allow merchant to access merchant restricted pages' do
-      sharon = merchants(:sharon)
-      perform_login(sharon)
       get new_product_path
+
+
       expect(session[:merchant_id]).must_equal sharon.id
+      expect{find_merchant}.must_equal 
       must_respond_with :success
     end
   end
