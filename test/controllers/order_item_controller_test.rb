8899a672595461752b02b5a109e17f7ebb8d59d7
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-06-18 12:48:15 -0700

    destroy test passing

--- test/controllers/order_item_controller_test.rb
+++ test/controllers/order_item_controller_test.rb
@@ -1,4 +1,12 @@
 require "test_helper"
 
 describe OrderItemsController do
+    describe "destroy" do 
+        it "can destroy order item" do 
+            order = orders(:nataliyas_order)
+            order_item = order_items(:gear_orderitem)
+            expect{delete order_order_item_path(order, order_item)}.must_differ "OrderItem.count", -1
+            must_redirect_to cart_path
+        end 
+    end 
 end
